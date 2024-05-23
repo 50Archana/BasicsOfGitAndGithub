1d4f7746b3807b21e9f39e26c3e86f777f1d7978
Archana <archukumari@5455gmail.com>
2024-05-23 16:23:35 +0530

    Second commit

--- basic.java
+++ basic.java
@@ -1,7 +1,15 @@
 public class basic{
     public static void main(String[] args){
      System.out.println("Hey!I am ready to learn git......");
-     int a=20;
+    
 
+         System.out.println("Hey!I am ready to learn github......");
+         int i=99;
+         static int fun (int n){
+            return n+1;
+         }
+         static int fun1 (int n){
+            return 34;
+         }
     }
 }
